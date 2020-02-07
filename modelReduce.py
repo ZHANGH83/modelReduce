c1cb5907f80a7fe0f4d8421778103f12736838d7
ZHANGH83 <60178933+ZHANGH83@users.noreply.github.com>
2020-02-07 22:03:19 +0000

    Add files via upload

---
+++ modelReduce.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from operator import itemgetter
+import sys
+
+current_word = None
+current_count = 0
+word = None
+
+# input comes from STDIN
+for line in sys.stdin:
+    line = line.strip() # remove leading and trailing whitespace
+    line = line.split(':') # remove
+    line = line[1]
+    word, count = line.split('\t', 1) # parse the input we got from mapper.py
+
+    # convert count (currently a string) to int
+    try:
+        count = int(count)
+    except ValueError:
+        # count was not a number, so silently
+        # ignore/discard this line
+        continue
+
+    # this IF-switch only works because map output is sorted
+    # by key (here: word) before it is passed to the reducer
+    if current_word == word:
+        current_count += count
+    else:
+        if current_word:
+            # write result to STDOUT
+            print('%s\t%s' % (current_word, current_count))
+        current_count = count
+        current_word = word
+
+# do not forget to output the last word if needed!
+if current_word == word:
+    print('%s\t%s' % (current_word, current_count))
